047a718b846c19c7a8f428771f226f7b6b1a712b
adam <adam@kek.industries>
2018-08-27 20:08:34 +0000

    lol

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"strings"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -12,25 +13,44 @@ func check(e error) {
 		panic(e)
 	}
 }
+func pathFromFlag() string {
+	path := flag.String("f", "", "file flag")
+	flag.Parse()
+	return *path
+}
+
+func pathFromEnv() string {
+	return os.Getenv("FILE")
+}
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("You must specify one arg as a file path")
+	path := pathFromFlag()
+	if path == "" {
+		path = pathFromEnv()
+	}
+
+	if path == "" {
+		fmt.Println("flag -f for files or env variable FILE")
 		os.Exit(1)
 	}
-	arg := os.Args[1]
-	proverbs, err := ioutil.ReadFile(arg)
+
+	proverbs, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println("failed to read file :%s" , err)
+		os.Exit(1)
+	}
+
 	sproverbs := string(proverbs)
 	check(err)
 
-  lines := strings.Split(sproverbs, "\n")
-  for _, l := range lines {
-	  fmt.Printf("%s\n", l)
-	  for k, v := range charCount(l) {
-		  fmt.Printf("'%c'=%d, ", k, v)
-	  }
-	  fmt.Print("\n\n")
-  }
+	lines := strings.Split(sproverbs, "\n")
+	for _, l := range lines {
+		fmt.Printf("%s\n", l)
+		for k, v := range charCount(l) {
+			fmt.Printf("'%c'=%d, ", k, v)
+		}
+		fmt.Print("\n\n")
+	}
 }
 func charCount(line string) map[rune]int {
 	m := make(map[rune]int, 0)
