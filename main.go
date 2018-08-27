4b783b241b06486b0e463888a91296f71d7ef16f
adam <adam@kek.industries>
2018-08-27 20:33:35 +0000

    overwrote with example

--- main.go
+++ main.go
@@ -8,11 +8,50 @@ import (
 	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+type proverb struct {
+	line  string
+	chars map[rune]int
+}
+
+func (p *proverb) charCount() map[rune]int {
+	if p.chars != nil {
+		return p.chars
+	}
+
+	m := make(map[rune]int, 0)
+	for _, c := range p.line {
+		m[c] = m[c] + 1
+	}
+	p.chars = m
+	return p.chars
+}
+
+func main() {
+	path := pathFromFlag()
+	if path == "" {
+		path = pathFromEnv()
+	}
+
+	if path == "" {
+		fmt.Println("You must specify one the file path with -f or as FILE environment variable.")
+		os.Exit(1)
+	}
+
+	proverbs, err := loadProverbs(path)
+	if err != nil {
+		fmt.Printf("Failed to load proverbs: %s", err)
+		os.Exit(1)
+	}
+
+	for _, p := range proverbs {
+		fmt.Printf("%s\n", p.line)
+		for k, v := range p.charCount() {
+			fmt.Printf("'%c'=%d, ", k, v)
+		}
+		fmt.Print("\n\n")
 	}
 }
+
 func pathFromFlag() string {
 	path := flag.String("f", "", "file flag")
 	flag.Parse()
@@ -23,39 +62,19 @@ func pathFromEnv() string {
 	return os.Getenv("FILE")
 }
 
-func main() {
-	path := pathFromFlag()
-	if path == "" {
-		path = pathFromEnv()
-	}
+func loadProverbs(path string) ([]*proverb, error) {
+	var proverbs []*proverb
 
-	if path == "" {
-		fmt.Println("flag -f for files or env variable FILE")
-		os.Exit(1)
-	}
-
-	proverbs, err := ioutil.ReadFile(path)
+	bs, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Println("failed to read file :%s" , err)
-		os.Exit(1)
+		return nil, err
 	}
 
-	sproverbs := string(proverbs)
-	check(err)
+	lines := strings.Split(string(bs), "\n")
+	for _, line := range lines {
+		p := &proverb{line: line}
+		proverbs = append(proverbs, p)
+	}
 
-	lines := strings.Split(sproverbs, "\n")
-	for _, l := range lines {
-		fmt.Printf("%s\n", l)
-		for k, v := range charCount(l) {
-			fmt.Printf("'%c'=%d, ", k, v)
-		}
-		fmt.Print("\n\n")
-	}
-}
-func charCount(line string) map[rune]int {
-	m := make(map[rune]int, 0)
-	for _, c := range line {
-		m[c] = m[c] + 1
-	}
-	return m
+	return proverbs, nil
 }
